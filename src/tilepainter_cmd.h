f10ff44eda012c6c4d1dd1266269c4cefe10af19
Tyler Trahan <tyler@tylertrahan.com>
2026-07-09 20:45:02 -0400

    Feature: Paint ground tiles (WIP, needs GUI)

---
+++ src/tilepainter_cmd.h
@@ -0,0 +1,24 @@
+/*
+ * This file is part of OpenTTD.
+ * OpenTTD is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+ * OpenTTD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <https://www.gnu.org/licenses/old-licenses/gpl-2.0>.
+ */
+
+/** @file tilepainter_cmd.h Command definitions related to tile painter. */
+
+#ifndef TILEPAINTER_CMD_H
+#define TILEPAINTER_CMD_H
+
+#include "tile_type.h"
+#include "command_type.h"
+#include "tilepainter.h"
+
+bool TileCanBePainted(TileIndex tile);
+void ClearPaintedTile(TileIndex tile);
+
+CommandCost CmdAddPrimaryPaint(TileIndex tile, TilePaint paint);
+CommandCost CmdRemovePrimaryPaint(TileIndex tile);
+
+
+#endif /* TILEPAINTER_CMD_H */
